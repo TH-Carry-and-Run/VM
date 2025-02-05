c192455131c95ce0b3a7c56475520273718cf472
서민재 <hey_minj@naver.com>
2025-02-05 12:40:00 +0900

    초기 생성

---
+++ src/main/java/com/hanbat/dotcar/DotcarApplication.java
@@ -0,0 +1,13 @@
+package com.hanbat.dotcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotcarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanbat/dotcar/DotcarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanbat.dotcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
